317c198b4ebfe37480014b12c067e6f2046258c8
Ashar Amir <ashharamir@gmail.com>
2024-07-03 18:25:44 +0300

    updated for heroku

--- summary.py
+++ summary.py
@@ -31,4 +31,9 @@ def summarizeText():
     return jsonify({'summary': response.text})
 
 if __name__ == "__main__":
-    app.run(port=5555, debug=True)
+    #USE THE BELOW FOR LOCALHOST
+    #app.run(port=5555, debug=True) 
+    
+    #use the below for HEROKU
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
