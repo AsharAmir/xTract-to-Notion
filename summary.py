e45230ecf28991567ce382d47a258afd692f1e07
Ashar Amir <ashharamir@gmail.com>
2024-07-03 18:47:08 +0300

    changed api key to @nu.edu

--- summary.py
+++ summary.py
@@ -3,7 +3,8 @@ from flask_cors import CORS
 import os
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyBSD1sFlgCwPIOH79HLI9dZC5Yi5wz3pgw")
+#genai.configure(api_key="AIzaSyBSD1sFlgCwPIOH79HLI9dZC5Yi5wz3pgw")
+genai.configure(api_key="AIzaSyApqyrYE3TfP8U0KaejCu7kvUNVNnNE_-Y")
 
 # Create the model
 generation_config = {
