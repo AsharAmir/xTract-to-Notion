4fbd0f799511bb151a5196cebe73d272b2038cdd
Ashar Amir <ashharamir@gmail.com>
2024-07-20 14:21:18 +0300

    Update summary.py

--- summary.py
+++ summary.py
@@ -54,5 +54,6 @@ if __name__ == "__main__":
     scheduler = BackgroundScheduler()
     scheduler.add_job(func=job_func, trigger="interval", minutes=14)
     scheduler.start()
+    print("Scheduler started")
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
